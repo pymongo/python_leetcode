83950dd920bb9ea921721a7d50e03b4731fbfae3
吴翱翔 <pylint@yandex.com>
2020-06-21 16:52:28 +0800

    参考中位数的小技巧题解，写完了python解答

--- sorting/median_of_two_sorted_arrays.py
+++ sorting/median_of_two_sorted_arrays.py
@@ -3,19 +3,41 @@ from typing import List
 
 
 def solution(a: List[int], b: List[int]) -> float:
-    len_a, len_b = len(a), len(B)
+    len_a, len_b = len(a), len(b)
     # 确保A
     if len_a > len_b:
         return solution(b, a)
-    if len_a == 0:
-        if len_b % 2 == 0:
-            return (b[len_b // 2 - 1] + b[len_b // 2]) / 2.0
-        else:
-            return b[len_b // 2]
-    a_left, a_right = 0, len_a-1
+    # if len_a == 0:
+    #     if len_b % 2 == 0:
+    #         return (b[len_b // 2 - 1] + b[len_b // 2]) / 2.0
+    #     else:
+    #         return b[len_b // 2]
+    total_len = len_a + len_b
+    half_len = (total_len + 1) // 2
+
+    a_left, a_right = 0, len_a - 1
+    a_mid_right_index: int
+    b_mid_right_index: int
     while a_left < a_right:
-        a_mid = (a_left + a_right) // 2
-    return -1
+        # 如果数组a是奇数个，a_mid_right会是数组a正中间的元素
+        a_mid_right_index = (a_left + a_right + 1) // 2
+        b_mid_right_index = half_len - a_mid_right_index
+        # 如果a分隔线右边的元素比b分隔线左边的元素小
+        if a[a_mid_right_index] < b[b_mid_right_index - 1]:
+            # a分隔线左边元素太小了，a的分隔线需要右移
+            a_left = a_mid_right_index + 1
+        else:
+            # a分隔线左边元素太大了，a的分隔线需要左移
+            a_right = a_mid_right_index
+    a_mid_right_index = a_left
+    b_mid_right_index = half_len - a_mid_right_index
+    a_mid_left = (a[a_mid_right_index - 1] if a_mid_right_index >= 1 else float("-inf"))
+    b_mid_left = (b[b_mid_right_index - 1] if b_mid_right_index >= 1 else float("-inf"))
+    if total_len % 2 == 1:
+        return max(a_mid_left, b_mid_left)
+    a_mid_right = (a[a_mid_right_index] if a_mid_right_index < len_a else float("inf"))
+    b_mid_right = (b[b_mid_right_index] if b_mid_right_index < len_b else float("inf"))
+    return (max(a_mid_left, b_mid_left) + min(a_mid_right, b_mid_right)) / 2
 
 
 class Test(unittest.TestCase):
