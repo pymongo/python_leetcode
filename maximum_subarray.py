eab1e7d0c82771331d3b3abf0f6943068aa8df03
吴翱翔 <pylint@yandex.com>
2020-06-22 22:44:10 +0800

    修复了暴力遍历的几个Bug

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -9,21 +9,25 @@ class Solution:
     @staticmethod
     def max_sub_array(nums: List[int]) -> int:
         size: int = len(nums)
-        max_sum: int = 0
+        max_sum: int = nums[0]
         for i in range(size):
-            temp_sum = 0
-            for j in range(i, size):
+            temp_sum = nums[i]
+            for j in range(i+1, size):
                 temp_sum += nums[j]
-                if temp_sum > max_sum:
-                    max_sum = temp_sum
+                if max(temp_sum, nums[j]) > max_sum:
+                    max_sum = max(temp_sum, nums[j])
         return max_sum
 
 
 class Testing(unittest.TestCase):
     TEST_CASES: List[Tuple[List[int], int]] = [
-        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6)
+        ([-1, 0, -2], 0),
+        ([-2, 1], 1),
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+        ([1], 1),
     ]
 
     def test_solution(self):
         for input_array, expected in self.TEST_CASES[:]:
+            print(input_array, expected)
             self.assertEqual(expected, Solution.max_sub_array(input_array))
