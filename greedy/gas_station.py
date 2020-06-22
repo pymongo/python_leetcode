a6988c3f6a2e9961c63fdfba5feeee7abe70c7c6
吴翱翔 <pylint@yandex.com>
2020-06-22 11:13:51 +0800

    不想debug暴力遍历的问题了，花时间debug不如学贪心

--- greedy/gas_station.py
+++ greedy/gas_station.py
@@ -15,8 +15,10 @@ def brute_force(gas: List[int], cost: List[int]) -> int:
         gas_volume = gas[i]
         # 内层循环遍历len-1次，遍历的是线段而不是端点
         for j in range(1, stations_count):
-            next_station = (i+j) % stations_count
-            print(next_station)
+            # next_station = (i+j) % stations_count
+            # next_station = (i+j)
+            # if next_station > stations_count:
+            #     next_station -= stations_count
             # 遍历到最后一个(也就是出发点)时，不能重复加两次出发点的油量
             # if next_station == i:
             #     gas_volume -= cost[next_station]
@@ -27,17 +29,30 @@ def brute_force(gas: List[int], cost: List[int]) -> int:
                 break
             gas_volume += gas[next_station]
         else:
-            # will run only if the inner loop is exited normally
-            # return if inner loop no break
-            return i
+            # 如果内层for循环break了
+            # 返回值的加油站是从1开始编号
+            return i+1
     return -1
 
 
 class Testing(unittest.TestCase):
+    """
+    case: ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3)
+    从 3 号加油站(索引为 3 处)出发，可获得 4 升汽油。此时油箱有 = 0 + 4 = 4 升汽油
+    开往 4 号加油站，此时油箱有 4 - 1 + 5 = 8 升汽油
+    开往 0 号加油站，此时油箱有 8 - 2 + 1 = 7 升汽油
+    开往 1 号加油站，此时油箱有 7 - 3 + 2 = 6 升汽油
+    开往 2 号加油站，此时油箱有 6 - 4 + 3 = 5 升汽油
+    开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
+    因此，3 可为起始索引。
+
+    【注意】返回值的加油站是从1开始编号
+    """
     TEST_CASES: List[Tuple[List[int], List[int], int]] = [
         ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3),
         ([1, 1, 3, 1], [2, 2, 1, 1], 2),
         ([5, 8, 2, 8], [6, 5, 6, 6], 3),
+        ([2, 3, 4], [3, 4, 3], -1),
     ]
 
     def test_brute_force(self):
