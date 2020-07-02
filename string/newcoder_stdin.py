3d7a9f82c859a6aa8dc0d953c192e492d7db1d4d
吴翱翔 <pylint@yandex.com>
2020-07-02 10:43:37 +0800

    解决了牛客网中多行输入，每行有1个或多个值的读取问题

--- string/newcoder_stdin.py
+++ string/newcoder_stdin.py
@@ -2,6 +2,7 @@
 牛客网上面大部分题都不是像leetcode那样将解答写在Solution类的实例方法上
 而是 从stdin读取测试用例，再通过stdout输出返回值
 """
+import sys
 
 
 def int_array_split_whitespace():
@@ -15,21 +16,65 @@ def int_array_split_whitespace():
     print(nums)
 
 
-def int_array_split_newline():
+def int_array_split_newline_1():
     """
     [牛客网-在线编程-连续子数组最大和](https://www.nowcoder.com/practice/03d341fb6c9d42debcdd38d82a0a545c)
     提示: 在IDEA的运行窗口，要通过cmd+D发送EOF，因为Ctrl+D是Debug的快捷键。在Terminal上Ctrl+D就能发送EOF了
     输入样例:
     1
     2
-    3
     """
     import fileinput
     nums = [int(line) for line in fileinput.input()]
     print(nums)
-    pass
+
+
+def int_array_split_newline_2():
+    """
+    input()和sys.stdin.readline()一样只能从stdin读一行，遇到'\n'就结束，不能处理多行输入的测试用例
+    输入样例:
+    1
+    2
+    """
+    nums = [int(line) for line in sys.stdin.readlines()]
+    print(nums)
+
+
+def multi_lines_multi_value():
+    """
+    [牛客网-在线编程-连续子数组最大和](https://www.nowcoder.com/practice/03d341fb6c9d42debcdd38d82a0a545c)
+
+    输入描述:
+    d:欧式距离
+    N:用户数
+    之后的N行表示第0个用户到第N-1个用户的地理位置坐标
+
+    输入样例:
+    2.0
+    5
+    3.0 5.0
+    6.0 13.0
+    2.0 6.0
+    7.0 12.0
+    0.0 2.0
+
+    输出样例:
+    [[0, 2], [1, 3], [4]]
+    """
+    distance = float(sys.stdin.readline().rstrip('\n'))
+    n = float(sys.stdin.readline().rstrip('\n'))
+    coordinates = []
+    for line in sys.stdin.readlines():
+        values = line.rstrip('\n').split()
+        coordinates.append((float(values[0]), float(values[1])))
+    print(distance, n)
+    print(coordinates)
+    output = [[0, 2], [1, 3], [4]]
+    print(output)
 
 
 if __name__ == '__main__':
     # int_array_split_whitespace()
-    int_array_split_newline()
+    # int_array_split_newline_1()
+    # int_array_split_newline_2()
+    multi_lines_multi_value()
