e8c414ffd98f8c3e0084b7f880614b9967ac9020
吴翱翔 <pylint@yandex.com>
2020-07-03 21:03:54 +0800

    解决了二分查找陷入死循环的问题

--- sorting/binary_search.py
+++ sorting/binary_search.py
@@ -6,11 +6,11 @@ def solution(nums, target):
     size = len(nums)
     left, right = 0, size-1
     while left <= right:
-        middle = (left+right+1) // 2
+        middle = (left+right) // 2
         if nums[middle] > target:
-            left = middle
+            right = middle-1
         elif nums[middle] < target:
-            right = middle
+            left = middle+1
         else:
             return middle
     return -1
