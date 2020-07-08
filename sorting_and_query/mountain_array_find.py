935384a022656a5da08ea4623ef28e36bc0e500e
吴翱翔 <pylint@yandex.com>
2020-07-08 12:53:29 +0800

    即将AC山脉数组一题

--- sorting_and_query/mountain_array_find.py
+++ sorting_and_query/mountain_array_find.py
@@ -11,15 +11,24 @@ import unittest
 from typing import List
 
 
-# class MountainArray:
-#     def __init__(self, nums: List[int]):
-#         self.nums = nums
-#
-#     def get(self, index: int) -> int:
-#         return self.nums[index]
-#
-#     def length(self) -> int:
-#         return len(self.nums)
+# 不能想着先将数组转化为List[int]再去处理
+# 很容易出WA: You made too many calls to MountainArray.get().
+class MountainArray:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def get(self, index: int) -> int:
+        return self.nums[index]
+
+    def length(self) -> int:
+        return len(self.nums)
+
+
+def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+    nums: List[int] = []
+    for i in range(0, mountain_arr.length()):
+        nums.append(mountain_arr.get(i))
+    return find_in_mountain_array(nums, target)
 
 
 def get_peak_index(nums: List[int]) -> int:
@@ -49,23 +58,26 @@ def binary_search(nums: List[int], target: int) -> int:
 def find_in_mountain_array(nums: List[int], target: int) -> int:
     length = len(nums)
     peak_index = get_peak_index(nums)
-    bsearch_left = binary_search(nums[0:peak_index], target)
+    #  print(nums[0:peak_index+1], nums[peak_index+1:length])
+    bsearch_left = binary_search(nums[0:peak_index + 1], target)
     if bsearch_left > -1:
         return bsearch_left
-    bsearch_right = binary_search(nums[peak_index+1:length][::-1], target)
+    bsearch_right = binary_search(nums[peak_index + 1:length][::-1], target)
     if bsearch_right > -1:
-        # 注意要加上原数组middle索引值作为偏移量
-        return peak_index + 1 + bsearch_right
+        # 注意右边部分时逆序后进行二分搜索，需要转换为原数组中的索引
+        return length - bsearch_right - 1
     return -1
 
 
 class UnitTest(unittest.TestCase):
     TEST_CASES = [
         ([1, 5, 2], 2, 2),
+        ([1, 5, 2], 5, 1),
         ([1, 2, 3, 4, 5, 3, 1], 3, 2),
         ([0, 1, 2, 4, 2, 1], 3, -1),
     ]
 
     def test_find_in_mountain_array(self):
         for nums, target, expected in self.TEST_CASES:
+            print(nums, target)
             self.assertEqual(expected, find_in_mountain_array(nums, target))
