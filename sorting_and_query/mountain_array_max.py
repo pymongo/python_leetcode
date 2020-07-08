7455cee726808639158fec0e5097b0b91ad700cd
吴翱翔 <pylint@yandex.com>
2020-07-08 10:36:41 +0800

    添加了mountain array的二分解法，还有Bug未解决

--- sorting_and_query/mountain_array_max.py
+++ sorting_and_query/mountain_array_max.py
@@ -9,17 +9,28 @@ from typing import List
 
 def peak_index_brute_force(nums: List[int]) -> int:
     i = 0
-    while nums[i] < nums[i+1]:
+    while nums[i] < nums[i + 1]:
         i += 1
     return i
 
 
 def peak_index(nums: List[int]) -> int:
-    return -1
+    start, end = 0, len(nums) - 1
+    while start <= end:
+        middle = start + (end - start) // 2
+        print(start, middle, end)
+        if nums[middle] > nums[start]:
+            # 山峰在[middle,end]之间，包含middle
+            start = middle
+        else:
+            # 山峰在[start,middle)
+            end = middle - 1
+    return start
 
 
 class UnitTest(unittest.TestCase):
     TEST_CASES = [
+        ([24, 69, 100, 99, 79, 78, 67, 36, 26, 19], 2),
         ([0, 1, 0], 1),
         ([0, 2, 1, 0], 1),
     ]
