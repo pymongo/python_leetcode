7beee74111286d75f8cf26a3efe892373bc9a9b2
吴翱翔 <pylint@yandex.com>
2020-07-17 23:14:39 +0800

    添加了借助i32中最大的3的幂的解法

--- bitwise/is_power_of_3.py
+++ bitwise/is_power_of_3.py
@@ -12,6 +12,23 @@
 3. 二进制最高位是1: ?
 
 获取从右边往左第一位非0 bit的索引位置的方法: math.log2(n&-n)+1
+
+# 官方解答:
+
+1. 转换为3进制后做正则
+// Integer.toString(n, 3)表示将n转为3进制的字符串
+// 字符串是否以1 ^1 开头，后跟 0 或 多个 0 0* 并且不包含任何其他值
+return Integer.toString(n, 3).matches("^10*$");
+2. 使用库函数log
+if n <= 0:
+    return False
+# >>> math.log(243, 3)
+# 4.999999999999999
+return math.log(n, 3).is_integer()
+3. 借助i32中最大的3的幂(3^19)
+因为 3 是质数，所以 3^19(i32范围内最大的3的幂)的除数只有 3^0, 3^1, ...
+因此我们只需要将 3^19 除以 n 若余数为 0 意味着 n 是 3^19 的除数，因此是 3 的幂
+return n > 0 && 1162261467 % n == 0;
 """
 
 import unittest
@@ -30,11 +47,10 @@ def my_bitwise(n: int) -> bool:
     # 二进制最后一位是1
     if (n & -n) != 1:
         return False
-    while n > 3:
-        if n % 3 != 0:
-            return False
-        n //= 3
-    return True
+    # power of n题型的万能模板
+    while n % 3 == 0:
+        n = n // 3
+    return n == 1
 
 
 class Testing(unittest.TestCase):
