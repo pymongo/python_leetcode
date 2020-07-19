1219b2d4dc40811f2b5a8a041fafc1ce456acbec
吴翱翔 <pylint@yandex.com>
2020-07-19 17:04:43 +0800

    AC了zigzag一题

--- string/zigzag_conversion.py
+++ string/zigzag_conversion.py
@@ -7,8 +7,32 @@ import unittest
 
 
 def solution(s: str, rows: int) -> str:
+    if rows == 1:
+        return s
+    size = len(s)
+    arr = [['']*size for _ in range(rows)]
+    # N型号填值方向，is_up=true表示斜着向上
+    is_up = False
+    i, j = 0, 0
+    for char in s:
+        if is_up and i == 0:
+            is_up = False
+        if not is_up and i == rows-1:
+            is_up = True
 
-    return ""
+        arr[i][j] = char
+
+        if is_up:
+            i -= 1
+            j += 1
+        else:
+            i += 1
+
+    result = ""
+    for row in arr:
+        # print(row)
+        result += "".join(row)
+    return result
 
 
 class Testing(unittest.TestCase):
