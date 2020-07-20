69f001a8442566b783786ba25f0f37f5c6ef358a
吴翱翔 <pylint@yandex.com>
2020-07-20 16:38:14 +0800

    AC了merge_two_sorted_arrays标准版

--- sorting_and_query/merge_two_sorted_arrays.py
+++ sorting_and_query/merge_two_sorted_arrays.py
@@ -6,6 +6,8 @@ import unittest
 from typing import List, Tuple
 
 
+# https://www.lintcode.com/problem/merge-two-sorted-arrays/
+# lintcode特有题，跟leetcode原版的区别是开辟一个新数组去返回
 def merge_two_sorted_arrays(nums1: List[int], nums2: List[int]) -> List[int]:
     size1, size2 = len(nums1), len(nums2)
     result: List[int] = []
@@ -23,13 +25,49 @@ def merge_two_sorted_arrays(nums1: List[int], nums2: List[int]) -> List[int]:
     return result
 
 
+# https://www.lintcode.com/problem/merge-sorted-array/
+# 与leetcode的merge-two-sorted-arrays一题基本一样，为了保证In-place还是要从后往前填值
+# 参考内存从地址[0,2]复制到地址[1,3]也是从后往前复制避免正序复制时指针遍历[1,2]时刚写入的新数据会被覆盖掉
+def merge_nums2_to_nums1(nums1: List[int], m: int, nums2: List[int], n):
+    ptr1, ptr2 = m - 1, n - 1
+    # 从后往前赋值
+    idx = m + n - 1
+    while ptr1 >= 0 and ptr2 >= 0:
+        if nums1[ptr1] > nums2[ptr2]:
+            nums1[idx] = nums1[ptr1]
+            ptr1 -= 1
+        else:
+            nums1[idx] = nums2[ptr2]
+            ptr2 -= 1
+        idx -= 1
+    while ptr1 >= 0:
+        nums1[idx] = nums1[ptr1]
+        ptr1 -= 1
+        idx -= 1
+    while ptr2 >= 0:
+        nums1[idx] = nums2[ptr2]
+        ptr2 -= 1
+        idx -= 1
+
+
 class Testing(unittest.TestCase):
     TEST_CASES: List[Tuple[List[int], List[int], List[int]]] = [
-        ([1, 2, 3], [2, 5, 6], [1, 2, 2, 3, 5, 6]),
-        ([2], [1], [1, 2]),
         ([], [1], [1]),
+        ([2], [1], [1, 2]),
+        ([1, 2, 3], [2, 5, 6], [1, 2, 2, 3, 5, 6]),
+    ]
+
+    TEST_CASES_2 = [
+        ([2, 0], 1, [1], 1, [1, 2]),
+        ([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6]),
+        ([0], 0, [1], 1, [1]),
     ]
 
     def test(self):
         for nums1, nums2, expected in self.TEST_CASES[:]:
             self.assertEqual(expected, merge_two_sorted_arrays(nums1, nums2))
+
+    def test_2(self):
+        for nums1, m, nums2, n, expected in self.TEST_CASES_2:
+            merge_nums2_to_nums1(nums1, m, nums2, n)
+            self.assertEqual(expected, nums1)
