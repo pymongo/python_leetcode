187ed4a2a2c742093d29a6147b890179ff055aff
吴翱翔 <pylint@yandex.com>
2020-07-22 16:01:01 +0800

    初步写完二叉树反序列化的方法

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -1,7 +1,7 @@
 import unittest
 import collections
 import pickle
-from typing import Optional
+from typing import List, Optional
 
 
 class TreeNode:
@@ -24,8 +24,26 @@ class TreeNode:
                 continue
         pass
 
+    # noinspection DuplicatedCode
+    def serialize(self) -> bytes:
+        if self is None:
+            return pickle.dumps([None])
+        queue = collections.deque()
+        queue.append(self)
+        binary_tree_arr: List[Optional[int]] = []
+        while queue:
+            node = queue.popleft()
+            if node is None:
+                binary_tree_arr.append(None)
+                continue
+            binary_tree_arr.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        return pickle.dumps(binary_tree_arr)
+
+    # noinspection DuplicatedCode
     @staticmethod
-    def serialize(root: 'TreeNode') -> bytes:
+    def static_serialize(root: 'TreeNode') -> bytes:
         """
         想法: 将二叉树序列化为"满"二叉树的一维数组，数组第2-3项表示第二层，以此类推
         level index     explain
@@ -37,7 +55,7 @@ class TreeNode:
             return pickle.dumps([None])
         queue = collections.deque()
         queue.append(root)
-        binary_tree_arr = []
+        binary_tree_arr: List[Optional[int]] = []
         while queue:
             node = queue.popleft()
             if node is None:
@@ -52,10 +70,33 @@ class TreeNode:
 
     @staticmethod
     def deserialize(data: bytes) -> Optional['TreeNode']:
-        arr = pickle.dumps(data)
+        arr: List[Optional[int]] = pickle.loads(data)
         if arr[0] is None:
             return None
+        root: TreeNode = TreeNode(arr[0])
+        # 队列存储的是还不清楚有没有左子树或右子树的节点
+        queue = collections.deque()
+        queue.append(root)
 
+        cursor: int = 1
+        size = len(arr)
+        while cursor < size:
+            # 考察node的左右子树
+            node: TreeNode = queue.popleft()
+            left_val = arr[cursor]
+            right_val = arr[cursor + 1]
+            if left_val:
+                left_node = TreeNode(left_val)
+                node.left = left_node
+                # 尚不清楚left_node有没有子树
+                queue.append(left_node)
+            if right_val:
+                right_node = TreeNode(right_val)
+                node.right = right_node
+                # 尚不清楚right_node有没有子树
+                queue.append(right_node)
+            cursor += 2
+        return root
 
 
 class TestTreeNode(unittest.TestCase):
@@ -65,6 +106,6 @@ class TestTreeNode(unittest.TestCase):
         root.right = TreeNode(3)
         root.left.left = TreeNode(4)
         root.left.right = TreeNode(5)
-        a: bytes = TreeNode.serialize(root)
-        b = pickle.loads(a)
-        print(b)
+        a: bytes = TreeNode.static_serialize(root)
+        b = TreeNode.deserialize(a)
+        print(b.val)
