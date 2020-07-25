3da934522406416f5e4f7b19728eb8aa8e3ba233
吴翱翔 <pylint@yandex.com>
2020-07-25 11:06:15 +0800

    AC了一半的测试用例

--- binary_tree/bst_closest_value_2.py
+++ binary_tree/bst_closest_value_2.py
@@ -10,6 +10,7 @@ def bst_closest_1(root: TreeNode, target: float, k: int) -> List[int]:
     if root is None:
         return result
     in_order = []
+    size = 0
     stack = collections.deque()
     curr = root
     while stack or curr:
@@ -18,13 +19,24 @@ def bst_closest_1(root: TreeNode, target: float, k: int) -> List[int]:
             curr = curr.left
         node = stack.pop()
         in_order.append(node.val)
+        size += 1
         curr = node.right
-    return in_order
+
+    # 第二部分，在中序遍历的有序数组中查找与target最接近的k项，参考[Find K Closest Elements]这题
+    start, end = 0, size-k+1
+    while start + 1 < end:
+        mid = start + (end - start) // 2
+        if target - in_order[mid] > in_order[mid+k-1] - target:
+            start = mid
+        else:
+            end = mid
+    return in_order[start:start+k]
 
 
 class Testing(unittest.TestCase):
     TEST_CASES = [
         ("3(1()(2))(4)", 0.275000, 2, [1, 2]),
+        ("2(1)(3)", 5.571429, 2, [2, 3]),
     ]
 
     def test_bst_closest_1(self):
