cec4d0e838b35eb1fece38c776f48ac1834a15b1
吴翱翔 <pylint@yandex.com>
2020-07-25 21:47:44 +0800

    优化了骑士最短路径的代码

--- bfs/knight_shortest_path.py
+++ bfs/knight_shortest_path.py
@@ -3,10 +3,17 @@ import collections
 import sys
 from typing import List
 
+
 class Point:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+
+
+HORSE_DIRECTIONS = [
+    (1, 2), (1, -2), (-1, 2), (-1, -2),
+    (2, 1), (2, -1), (-2, 1), (-2, -1)
+]
 
 
 def my_bfs(grid: List[List[int]], source: Point, destination: Point) -> int:
@@ -16,7 +23,6 @@ def my_bfs(grid: List[List[int]], source: Point, destination: Point) -> int:
     """
     rows = len(grid)
     cols = len(grid[0])
-    min_steps = sys.maxsize
     queue = collections.deque()
     queue.append((source.x, source.y, 0))
     while queue:
@@ -28,21 +34,28 @@ def my_bfs(grid: List[List[int]], source: Point, destination: Point) -> int:
         grid[x][y] = 1
         if x == destination.x and y == destination.y:
             # min_steps = min(min_steps, steps)
-            return min_steps
-        # 优化1. 将马跳跃的方向(dx, dy)抽取为常量，将判断是否越界的代码抽取成子函数
-        # 优化2. 先判断越界后加入队列，避免错误的点入栈又出栈消耗性能
-        # 优化3. 可以使用distance = {(source.x, source.y): 0}存储步骤数
-        queue.append((x + 1, y + 2, steps + 1))
-        queue.append((x + 1, y - 2, steps + 1))
-        queue.append((x - 1, y + 2, steps + 1))
-        queue.append((x - 1, y - 2, steps + 1))
-        queue.append((x + 2, y + 1, steps + 1))
-        queue.append((x + 2, y - 1, steps + 1))
-        queue.append((x - 2, y + 1, steps + 1))
-        queue.append((x - 2, y - 1, steps + 1))
-    if min_steps == sys.maxsize:
-        return -1
-    return min_steps
+            return steps
+        next_steps = steps + 1
+        for dx, dy in HORSE_DIRECTIONS:
+            next_x, next_y = x + dx, y + dy
+            if -1 < next_x < rows and -1 < next_y < cols:
+                queue.append((next_x, next_y, next_steps))
+    return -1
+
+
+"""
+# 优化1. 将马跳跃的方向(dx, dy)抽取为常量，将判断是否越界的代码抽取成子函数
+# 优化2. 先判断越界后加入队列，避免错误的点入栈又出栈消耗性能
+# 优化3. 可以使用distance = {(source.x, source.y): 0}存储步骤数
+queue.append((x + 1, y + 2, steps + 1))
+queue.append((x + 1, y - 2, steps + 1))
+queue.append((x - 1, y + 2, steps + 1))
+queue.append((x - 1, y - 2, steps + 1))
+queue.append((x + 2, y + 1, steps + 1))
+queue.append((x + 2, y - 1, steps + 1))
+queue.append((x - 2, y + 1, steps + 1))
+queue.append((x - 2, y - 1, steps + 1))
+"""
 
 
 class Testing(unittest.TestCase):
