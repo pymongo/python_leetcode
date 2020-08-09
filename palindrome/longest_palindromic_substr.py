d71b5c7c79842f78f4595051e6ffa661a27fe580
吴翱翔 <pylint@yandex.com>
2020-08-09 17:43:57 +0800

    临摹了区间型动态规划，外层循环是子串长度斜着填表的思路

--- palindrome/longest_palindromic_substr.py
+++ palindrome/longest_palindromic_substr.py
@@ -40,9 +40,20 @@ Weak Hire相当于弃权票，三轮面试中，2次给了Weak Hire，1次No Hir
 
 ### Strong No Hire
 一种算法都想不到，或者用暴力遍历结果超时
+
+DP解法填表顺序注意事项:
+除了i和j的其中一个要倒着遍历，另一种解决思路是，本质上解决的思路是
+「先循环区间长度，再循环区间的起点」
+for length in range(3, size+1):
+    for i in range(size-length+1):
+        j = i+length-1
+        dp[i][j] = dp[i+1][j-1] and s[i] == s[j]
+注意这种写法一定要把子串s[i]的两侧s[i+1]和s[i-1]也初始化了，也就是所有长度为2的子串都初始化了
 """
 import unittest
 from typing import List, Tuple
+
+
 # from mydbg import dbg
 
 
@@ -53,7 +64,7 @@ def expand_center(s: str, size: int, left: int, right: int) -> (int, int):
 
     # 如果左边和右边不相等，例如ab，它不是个回文串，应该返回1
     # 由于跳出循环时哪怕两边不相等也会额外扩散一次，所以返回值要往里收缩一次
-    return left+1, right-1
+    return left + 1, right - 1
 
 
 def longest_palindromic_substr(s: str) -> str:
@@ -79,7 +90,37 @@ def longest_palindromic_substr(s: str) -> str:
             max_len, max_len_left, max_len_right = odd_len, odd_left, odd_right
         if even_len > max_len:
             max_len, max_len_left, max_len_right = even_len, even_left, even_right
-    return s[max_len_left:max_len_right+1]
+    return s[max_len_left:max_len_right + 1]
+
+
+class Solution:
+    @staticmethod
+    def dp_solution(s: str) -> str:
+        size = len(s)
+        max_len = 0
+        max_start = 0
+        # dp[i][j]表示子串s[i][j]是不是回文串
+        dp = [[False] * size for _ in range(size)]
+        # TODO 由于填表时依赖左下角的值，所以区间型动态规划最佳的填表方向是「左上-右下」对角线那样斜着填
+        # 枚举子串的长度(枚举区间型动态规划的长度)
+        for length in range(1, size + 1):
+            # 枚举子串的起始位置i
+            for i in range(size - length + 1):
+                j = i + length - 1
+                if length == 1:
+                    # 左上右下对角线
+                    dp[i][j] = True
+                elif length == 2:
+                    # 这是填表时的第二条斜线
+                    # 长度为2的字符串，只看头尾是否相等判断是不是回文串
+                    dp[i][j] = s[i] == s[j]
+                else:
+                    dp[i][j] = dp[i + 1][j - 1] and s[i] == s[j]
+                # 找到一个回文子串就要判断是否要更新最大值
+                if dp[i][j] and length > max_len:
+                    max_len = length
+                    max_start = i
+        return s[max_start:max_start + max_len]
 
 
 class Testing(unittest.TestCase):
@@ -95,3 +136,7 @@ class Testing(unittest.TestCase):
     def test(self):
         for input_str, expected1, expected2 in self.TEST_CASES[:]:
             self.assertIn(longest_palindromic_substr(input_str), [expected1, expected2])
+
+    def test_dp_solution(self):
+        for input_str, expected1, expected2 in self.TEST_CASES[:]:
+            self.assertIn(Solution.dp_solution(input_str), [expected1, expected2])
