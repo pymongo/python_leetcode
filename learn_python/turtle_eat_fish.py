820fb4195928390cdebfd95a40b176df5c93df74
吴翱翔 <pylint@yandex.com>
2020-08-14 18:05:20 +0800

    优化了turtle动画中鱼随机移动的算法

--- learn_python/turtle_eat_fish.py
+++ learn_python/turtle_eat_fish.py
@@ -1,29 +1,25 @@
+"""
+python -m turtledemo
+python -m turtledemo.clock
+"""
 import turtle  # 官方Demo: python -m turtledemo.clock
 from time import strftime
-from random import randint, choice
+import random
 
 GRID_WIDTH = 60
 ROWS = 10
 FONT = ('Meiryo', 20, 'normal')
 
-# 设置turtle画板尺寸和窗口尺寸
-turtle.screensize(GRID_WIDTH*10, GRID_WIDTH*10+90)  # get canvasSize: turtle.screensize()
-turtle.setup(GRID_WIDTH*10+120, GRID_WIDTH*10+120)  # get windowSize: turtle.window_width()
-turtle.title("Turtle Eat Fish Animation")
-
+# TODO 隐藏全局所有turtle路径的暂时只能放在这，不然函数里的turtle画图全都有路径
 turtle.tracer(False)
 
-# 设置turtle光标默认属性
-turtle.speed('fast')
-turtle.penup()
-turtle.hideturtle()
 
 # 用turtle.turtles()检验是否销毁了对象
 def draw_grid():
     t_row = turtle.clone()
     turtle.tracer(False)
     # 将光标移动到画板的左上角
-    t_row.setpos(-5*GRID_WIDTH, 5*GRID_WIDTH)
+    t_row.setpos(-5 * GRID_WIDTH, 5 * GRID_WIDTH)
     t_row.pendown()
     t_col = t_row.clone()
     t_col.right(90)
@@ -31,12 +27,12 @@ def draw_grid():
         t_row.fd(GRID_WIDTH)
         t_clone = t_row.clone()
         t_clone.right(90)
-        t_clone.fd(10*GRID_WIDTH)
+        t_clone.fd(10 * GRID_WIDTH)
 
         t_col.fd(GRID_WIDTH)
         t_clone = t_col.clone()
         t_clone.left(90)
-        t_clone.fd(10*GRID_WIDTH)
+        t_clone.fd(10 * GRID_WIDTH)
     turtle.tracer(True)
     # 释放无用turtle对象内存
     del turtle.turtles()[1:]  # del c只会删除c本身
@@ -44,8 +40,8 @@ def draw_grid():
 
 def init_time():
     t = turtle.clone()
-    t.setpos(-5*GRID_WIDTH, -5*GRID_WIDTH-45)
-    t.write("当前时间:  ", move=True, font=FONT)
+    t.setpos(-5 * GRID_WIDTH, -5 * GRID_WIDTH - 45)
+    t.write("当前时间:  ", move=True)
     return t.pos()
 
 
@@ -54,45 +50,71 @@ def update_time():
     # 1. undo 2. 打印一段空格 3. clear(删除该对象所有笔迹)
     turtle.tracer(False)
     time_writer.clear()
-    time_writer.write(strftime('%H:%M:%S'), font=FONT)
+    time_writer.write(strftime('%H:%M:%S'))
     turtle.tracer(True)
     turtle.ontimer(update_time, 1000)
 
 
 class Fish:
-    # static
-    fish_template = turtle.clone()
-    fish_template.showturtle()
     # 考虑鱼在格子中间，故边界应该是 [-270, 270]
-    boundary = [-((ROWS/2)-0.5)*GRID_WIDTH, ((ROWS/2)-0.5)*GRID_WIDTH]
+    boundary = [-((ROWS / 2) - 0.5) * GRID_WIDTH, ((ROWS / 2) - 0.5) * GRID_WIDTH]
+    DIRECTIONS = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    HEADING = (0, 180, 90, -90)
 
     def __init__(self):
-        self.t = Fish.fish_template.clone()
-        self.x = Fish.boundary[0] + GRID_WIDTH*randint(0, ROWS-1)
-        self.y = Fish.boundary[0] + GRID_WIDTH*randint(0, ROWS-1)
+        self.t = turtle.Turtle()
+        self.t.penup()  # 不显示鱼🐟的移动路径
+        self.t.showturtle()
+        self.x = Fish.boundary[0] + GRID_WIDTH * random.randint(0, ROWS - 1)
+        self.y = Fish.boundary[0] + GRID_WIDTH * random.randint(0, ROWS - 1)
+
+        # 隐藏从原点移动到随机位置的动画
+        turtle.tracer(False)
         self.t.setpos(self.x, self.y)
+        turtle.tracer(True)
 
     def move(self):
-        choice_list = ['x + 1', 'x - 1', 'y + 1', 'y - 1']
-        if self.x == Fish.boundary[1]: choice_list.remove('x + 1')
-        if self.x == Fish.boundary[0]: choice_list.remove('x - 1')
-        if self.y == Fish.boundary[1]: choice_list.remove('y + 1')
-        if self.y == Fish.boundary[0]: choice_list.remove('y - 1')
-        choose = choice(choice_list)
-        if choose == 'x + 1':  self.x += GRID_WIDTH; self.t.setheading(0)
-        if choose == 'x - 1':  self.x -= GRID_WIDTH; self.t.setheading(180)
-        if choose == 'y + 1':  self.y += GRID_WIDTH; self.t.setheading(90)
-        if choose == 'y - 1':  self.y -= GRID_WIDTH; self.t.setheading(-90)
+        # dx, dy = -1, -1
+        # next_x, next_y = -1, -1
+        while True:
+            direction = random.randint(0, 3)
+            dx, dy = Fish.DIRECTIONS[direction]
+            next_x, next_y = self.x + dx * GRID_WIDTH, self.y + dy * GRID_WIDTH
+            if Fish.boundary[0] <= next_x <= Fish.boundary[1] and Fish.boundary[0] <= next_y <= Fish.boundary[1]:
+                self.x, self.y = next_x, next_y
+                break
+        self.t.setheading(Fish.HEADING[direction])
         self.t.setpos(self.x, self.y)
 
-def update_fish():
-    turtle.tracer(True)
-    for fish in fishs:
-        fish.move()
-    turtle.ontimer(update_fish, 500)
+
+def run_turtle_timer_event():
+    try:
+        for fish in fishes:
+            fish.move()
+        turtle.ontimer(run_turtle_timer_event, 500)
+    except turtle.Terminator as err:
+        print(err)
 
 
-try:
+def setup():
+    pass
+
+
+def main():
+    setup()
+    run_turtle_timer_event()
+
+
+if __name__ == '__main__':
+    # 设置turtle画板尺寸和窗口尺寸
+    turtle.getscreen().screensize(GRID_WIDTH * 10, GRID_WIDTH * 10 + 90)  # get canvasSize: turtle.screensize()
+    turtle.getscreen().setup(GRID_WIDTH * 10 + 120, GRID_WIDTH * 10 + 120)  # get windowSize: turtle.window_width()
+
+    # 设置turtle光标默认属性
+    turtle.speed('fast')
+    turtle.penup()
+    turtle.hideturtle()
+
     draw_grid()
     pos = init_time()  # 传坐标 防"当前时间"被clear
     # 清理上面函数产生的turtle对象
@@ -100,10 +122,9 @@ try:
     time_writer = turtle.clone()
     time_writer.setpos(pos)
 
-    fishs = []
+    fishes = []
     for i in range(6):
-        fishs.append(Fish())
-    Fish.fish_template.hideturtle()
+        fishes.append(Fish())
 
     # 定时更新部分 https://zhuanlan.zhihu.com/p/32094690
     # 用死循环不断调用定时器会一直占用线程
@@ -112,9 +133,7 @@ try:
     #
     # 另外一种方法，就是在回调函数中，创建定时器并启动，形成递归调用
 
-    update_time()
-    update_fish()
+    # update_time()
+    run_turtle_timer_event()
 
-    turtle.done()  # 等同于mainloop()
-except turtle.Terminator:
-    pass
+    turtle.mainloop()
