014038ac28d3956d8806ba84324cb88b9aefe23c
wuaoxiang <pylint@yandex.com>
2020-10-30 22:17:29 +0800

    remove reverse_integer solution

--- easy/jewels_and_stones.py
+++ easy/jewels_and_stones.py
@@ -1,7 +1,7 @@
 # 这种题无非就ASCII做数组或哈希表
 class Solution:
     @staticmethod
-    def numJewelsInStones(J: str, S: str) -> int:
+    def JewelsInStones(J: str, S: str) -> int:
         jewels = set(J)
         count = 0
         for stone in S:
